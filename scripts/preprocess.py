4f240795617c857677d89d8737020152f4b772c0
Paul <paul.bruneau.etu@univ-lemans.fr>
2020-12-11 15:14:29 +0100

    Modification of the preprocess programm so it works as  a python function

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -5,43 +5,52 @@ Created on Thu Dec  3 22:43:25 2020
 @author: Paul Bruneau
 """
 
-file = open('tab_4.tab')
-Lines = file.readlines()
 
-count = 0
+
+#function extractTuples
+#input : file
+#output : all the tuples(string, fret) of the file
+
+def extractTuples(file):
+
+    #file = open('D:\\Documents\\Ensim\\S9b\\ISP\\tg tab\\tab_test.tab')
+    Lines = file.readlines()
+                
+    standard_tuning = list("EBGDAE")
+    tuples = [] #(string, fret)
+    vecset = list("EBGDAE")
+    
+    for line in range(len(Lines)-6):
+        vec = []
+        for i in range(6):
+            vec.append(Lines[line + i])
+            vec[i] = vec[i][0]
+            if (vec == standard_tuning):
+                for i in range(line,line+6):
+                    vecset[i-line] += Lines[i]
+                    
+    vecset2 = [list(vecset[0]),
+               list(vecset[1]),
+               list(vecset[2]),
+               list(vecset[3]),
+               list(vecset[4]),
+               list(vecset[5])]
+    
+    i = 0
+    while (i < len(vecset2[0]) - 1):
+        vec1 = getSingleVec(vecset2, i)
+        vec2 = getSingleVec(vecset2, i+1)
+        if (vec1.count('-') == 5):
+            for element in range(6):
+                if (vec1[element] != '-'):
+                    if (vec2[element] != '-'):
+                        tuples.append([6-element, int(vec1[element]+vec2[element])])
+                        i += 1
+                    else:
+                        tuples.append([6-element, int(vec1[element])])
+        i += 1
+        
+    return tuples
 
 def getSingleVec(vecset, i):
     return[row[i] for row in vecset]
-
-        
-standard_tuning = list("EBGDAE")
-tuples = [] #(string, fret)
-vecset = list("EBGDAE")
-
-print(standard_tuning)
-for line in range(len(Lines)-6):
-    count += 1
-    vec = []
-    for i in range(6):
-        vec.append(Lines[line + i])
-        vec[i] = vec[i][0]
-        if (vec == standard_tuning):
-            print("ok!")
-            for i in range(line,line+6):
-                vecset[i-line] += Lines[i]
-                
-vecset2 = [list(vecset[0]),
-           list(vecset[1]),
-           list(vecset[2]),
-           list(vecset[3]),
-           list(vecset[4]),
-           list(vecset[5])]
-
-
-for i in range(len(vecset2[0])):
-    vec = getSingleVec(vecset2, i)
-    if (vec.count('-') == 5):
-        for element in range(6):
-            if (vec[element] != '-'):
-                tuples.append([6-element,vec[element]]) 
-    
